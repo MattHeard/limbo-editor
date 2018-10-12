dd4ad30eac8e567355e594bebdf2b1215a14c8c8
Matt Heard <matt@mattheard.net>
2018-10-12 19:53:35 +0200

    working

--- spec/list_tests_subcommand_spec.rb
+++ spec/list_tests_subcommand_spec.rb
@@ -1,5 +1,12 @@
 describe 'the list-tests subcommand' do
+  let(:bin_location) do
+    '/Users/matthew.heard/Development/Personal/limbo_editor/bin/led'
+  end
+
+  let(:invocation) { %x(#{bin_location} list-tests) }
+
   context 'without args' do
-    it 'describes itself'
+    it 'describes itself' do
+    end
   end
 end
