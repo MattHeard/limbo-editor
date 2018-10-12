33c562c61516f2fe0368e86c503cad581287dde6
Matt Heard <matt@mattheard.net>
2018-10-12 19:07:31 +0200

    working

--- spec/with_no_args_spec.rb
+++ spec/with_no_args_spec.rb
@@ -3,7 +3,7 @@ describe 'with no args' do
     '/Users/matthew.heard/Development/Personal/limbo_editor/bin/led'
   end
 
-  let(:invocation) { %x("#{bin_location}") }
+  let(:invocation) { %x(#{bin_location}) }
 
   it 'describes itself' do
     expect(invocation).to eq "Limbo EDitor\n"
